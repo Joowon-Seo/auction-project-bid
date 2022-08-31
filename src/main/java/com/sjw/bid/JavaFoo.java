85a9597d6bfcceef0c6d6f9a585dc96bb7bd9917
Juwon <tjwndnjs1998@gmail.com>
2022-09-01 01:02:34 +0900

    project settings 1. 프로젝트 생성 2. code convention 설정 3. jacoco 설정 4. gitignore 설정

---
+++ src/main/java/com/sjw/bid/JavaFoo.java
@@ -0,0 +1,20 @@
+package com.sjw.bid;
+
+public class JavaFoo {
+	public String hello(String name) {
+		switch (name) {
+			case "펭":
+				return "하";
+//			case "hello":
+//				return "world";
+			default:
+				return "no one";
+		}
+	}
+
+	public void callMe() {
+		System.out.println("Please, call me");
+	}
+
+}
+
