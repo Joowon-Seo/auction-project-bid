761fb75c2d160e5facb5d7c1cb5ce04781b5e872
Juwon <tjwndnjs1998@gmail.com>
2022-09-11 05:50:59 +0900

    feat : createUser Controller 생성
    /user/create에 대한 Controller 구현

--- src/main/java/com/sjw/bid/controller/UserController.java
+++ src/main/java/com/sjw/bid/controller/UserController.java
@@ -1,2 +1,25 @@
-package com.sjw.bid.controller;public class UserController {
+package com.sjw.bid.controller;
+
+import com.sjw.bid.dto.CreateUser;
+import com.sjw.bid.service.UserService;
+import javax.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+	private final UserService userService;
+
+	@PostMapping("/user/create")
+	private CreateUser.Response createUser(
+		@RequestBody @Valid CreateUser.Request request
+	) {
+		return CreateUser.Response.from(
+			userService.createUser(request)
+		);
+	}
 }
