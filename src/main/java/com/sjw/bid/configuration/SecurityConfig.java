25816f4c8b495e807be840ea1c5b9be07e584715
Juwon <tjwndnjs1998@gmail.com>
2022-09-11 17:24:40 +0900

    feat : SecurityConfig Configuration 생성
    Spring Security 설정을 위해 클래스 생성 및 설정

--- src/main/java/com/sjw/bid/configuration/SecurityConfig.java
+++ src/main/java/com/sjw/bid/configuration/SecurityConfig.java
@@ -1,2 +1,31 @@
-package com.sjw.bid.configuration;public class SecurityConfig {
+package com.sjw.bid.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable();
+		http
+			.authorizeRequests()
+				.antMatchers("/**","/user/create").permitAll();
+
+		super.configure(http);
+	}
+
+
 }
