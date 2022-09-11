71b878a7f30ab243b9efcf686336f901ea029188
Juwon <tjwndnjs1998@gmail.com>
2022-09-11 19:52:48 +0900

    feat : userException 예외처리 적용
    사용중인 이메일 인지 확인하여 존재한다면 예외 발생

--- src/main/java/com/sjw/bid/service/UserService.java
+++ src/main/java/com/sjw/bid/service/UserService.java
@@ -1,16 +1,19 @@
 package com.sjw.bid.service;
 
+import static com.sjw.bid.type.ErrorCode.THIS_EMAIL_ALREADY_EXISTS;
+
 import com.sjw.bid.domain.user.User;
-import com.sjw.bid.domain.user.UserLevel;
-import com.sjw.bid.domain.user.UserStatus;
 import com.sjw.bid.dto.CreateUser;
 import com.sjw.bid.dto.UserDto;
+import com.sjw.bid.exception.UserException;
 import com.sjw.bid.repository.UserRepository;
-import java.time.LocalDate;
+import com.sjw.bid.type.UserLevel;
+import com.sjw.bid.type.UserStatus;
 import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -18,8 +21,13 @@ public class UserService {
 
 	private final UserRepository userRepository;
 
+	@Transactional
 	public UserDto createUser(CreateUser.Request request) {
+
+		verificationEmailDuplication(request.getEmail());
+
 		String passwordEncryption = passwordEncryption(request.getPassword());
+
 		return UserDto.fromEntity(
 			userRepository.save(User.builder()
 				.name(request.getName())
@@ -36,8 +44,15 @@ public class UserService {
 		);
 	}
 
-	private String passwordEncryption(String password){
+	private String passwordEncryption(String password) {
 		return BCrypt.hashpw(password, BCrypt.gensalt());
 	}
+
+	private void verificationEmailDuplication(String email) {
+		if (userRepository.existsByEmail(email)) {
+			throw new UserException(THIS_EMAIL_ALREADY_EXISTS);
+		}
+	}
+
 
 }
